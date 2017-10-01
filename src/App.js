8f56493daf1602e9b830d21f2680f4da077fd3c6
Shadai Lopez <electronica_srlg@yahoo.com.mx>
2017-09-30 21:16:44 -0500

    State variables
    This is a basic example of change the state variables.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+  constructor(){
+    super();
+    this.state = {
+      message0: "hello world",
+      message1: "this is an example about state variables"
+    };
+    this.onClick = this.onClick.bind(this);
+  }
+    /*this function will change the value of the state*/
+    onClick(e){
+    this.setState({message1:"next state after click on text !!"});
+  }
+
+  render(){
+    return(
+      <div>
+        <h2 onClick={this.onClick}>{this.state.message0}</h2>
+        <h4>{this.state.message1}</h4>
       </div>
-    );
+    )
   }
 }
 
